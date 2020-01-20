f98a3ae01709f90ccd149f40ba52108fe3d8d0c6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-20 03:14:12 +0000

    first commit

---
+++ lib/best_cupcakes.rb
@@ -0,0 +1,6 @@
+require "best_cupcakes/version"
+
+module BestCupcakes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/best_cupcakes/version.rb
@@ -0,0 +1,3 @@
+module BestCupcakes
+  VERSION = "0.1.0"
+end
